796be84ef99b294ac063f94bba9f0d4eedb7d464
Cerq <66092565+cerqiest@users.noreply.github.com>
2023-04-02 12:55:50 -0400

    Completely revamp system

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 const fetch = require("node-fetch");
 
-function findVersion(data) {
-	return new Promise((resolve, reject) => {
-		const newLine = data.split("\n");
-		newLine.forEach(v => {
-			const split = v.split("=");
-			const keyword = split[0];
-			const value = split[1];
-			resolve(value);
-		})
-	})
+class InformationFileSyntaxException extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "InformationFileSyntaxException";
+  }
 }
 
-fetch("https://api.github.com/users/znci/repos").then(res => res.json()).then(json => {
-	json.forEach(element => {
-		let index = 0;
-		fetch(`https://raw.githubusercontent.com/znci/${element.name}/main/.znci`).then(res => res.text()).then(data => {
-			findVersion(data).then(version => {
-				if(version) {
-					console.log(`${element.name} - ${version}`);
-				}
-			})
-		})
-	});
-})
+function getDataAsObject(data) {
+  let lines = data.split("\n");
+  let obj = {};
+
+  lines.forEach((line) => {
+    if (line.startsWith("#")) return; // ignore comments
+    if (line.trim() === "") return; // ignore empty lines
+    if (!line.includes("=")) return; // ignore lines without an equal sign
+
+    let [key, ...value] = line.split("=");
+    value = value.join("=");
+
+    if (!value.startsWith('"') || !value.endsWith('"')) {
+      throw new InformationFileSyntaxException(
+        `Value for key with identifier '${key}' is not a valid string (missing quotes at beginning or end).`
+      );
+    }
+
+    if (value.length < 2) {
+      throw new InformationFileSyntaxException(
+        `Key with identifier '${key}' has no recognized value.`
+      );
+    }
+
+    obj[key] = value;
+  });
+
+  return obj;
+}
+
+fetch("https://api.github.com/users/znci/repos")
+  .then((res) => res.json())
+  .then((json) => {
+    json.forEach(async (element) => {
+      let request = await fetch(
+        `https://raw.githubusercontent.com/znci/${element.name}/main/.znci`
+      );
+
+      if (request.status === 200) {
+        let data = await request.text();
+        let obj = getDataAsObject(data);
+
+        console.log(obj);
+      }
+    });
+  });
